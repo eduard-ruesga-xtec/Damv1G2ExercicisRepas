6701cfde3cdf1da3e985fac7c528ba45db5e65dc
Eduard Ruesga C. <eduard.ruesga@itb.cat>
2025-01-08 12:43:56 +0100

    Feature: exercise 1 and call in main project #1

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -4,5 +4,23 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        GetSecondsPrintTime();
+    }
+
+    private static void GetSecondsPrintTime()
+    {
+        const string Msg = "Insereix número de segons";
+        int totalSeconds =0;
+
+        Console.WriteLine(Msg);
+        try
+        {
+            totalSeconds = int.Parse(Console.ReadLine());
+            Console.WriteLine(MyLibrary.Math.Seconds2HoursTime(totalSeconds));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
--- MyLibrary/Math.cs
+++ MyLibrary/Math.cs
@@ -1,7 +1,16 @@
 ﻿namespace MyLibrary
 {
-    public class Math
+    public static class Math
     {
+        public static string Seconds2HoursTime (int timeSeconds)
+        {
+            if (timeSeconds < 0) throw new Exception("S'espera un número positiu");
 
+            int hours = timeSeconds / 3600;
+            int minutes = (timeSeconds % 3600)/60;
+            int seconds = timeSeconds % 60;
+
+            return $"{hours}hores {minutes}minuts {seconds}segons";
+        }
     }
 }
